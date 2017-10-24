939f0dfffe047c6297d23f84919c5d55a7bd5ff4
Oldim <olifirenko88@gmail.com>
2017-10-24 13:27:46 +0200

    comm op while vb

--- javascript/basisstructuren/while02.js
+++ javascript/basisstructuren/while02.js
@@ -1,5 +1,9 @@
 'use strict';
 var i = 0;
 while( i++ < 10){
-    console.log(i);
+    console.log(i); //cijfers van 1 tem 10
+}
+
+while( ++i < 10){
+    console.log(i); //cijfers van 1 tem 9, want ++ is prefix
 }
