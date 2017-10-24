b8019e75d74ea0fa09521f6c1a3ae9b4d7fba795
Oldim <olifirenko88@gmail.com>
2017-10-24 12:01:51 +0200

    oef switch03

--- javascript/basisstructuren/switch.js
+++ javascript/basisstructuren/switch.js
@@ -15,3 +15,19 @@ switch (uur) {
     default:
         console.log("Werk maar rustig verder");
 }
+
+switch (uur) {
+    case 9:
+    case 10:
+        console.log("Tussen 9 en 10 kan je eens naar koffie zien");
+        break;
+    case 12:
+    case 13:
+        console.log("Tussen 12 en 1 eet iedereen");
+        break;
+    case 18:
+        console.log("Tussen 6 en 7 begint een werkmens te leven");
+        break;
+    default:
+        console.log("Werk maar rustig verder");
+}
