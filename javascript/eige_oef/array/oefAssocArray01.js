4ec6ed58f9d2652a48aea6635c4342db35a75819
Oldim <olifirenko88@gmail.com>
2017-11-22 14:09:51 +0100

    tset

--- javascript/eige_oef/array/oefAssocArray01.js
+++ javascript/eige_oef/array/oefAssocArray01.js
@@ -4,9 +4,8 @@ var toetsenbord = require('readline-sync');
 
 var lijst = new Array();
 var zin = toetsenbord.question("Geef een zin: ");
+zin= zin.replace(/[.,';:!?"]/g,"");
 var result = "";
-
-
 if (zin.charAt(i) != "," && zin.charAt(i) != " ") {
     // result = zin.charAt(i);
      result = zin.split(" ");
@@ -17,6 +16,10 @@ for (var i = 0; i < result.length; i++) {
     if (lijst[result[i]] == null) lijst[result[i]] = 0;
     lijst[result[i]]++;
 }
+
+console.log("Aantal woorden in de zin: %d", result.length);
+console.log("Aantal karakters in de zin: %d", zin.length);
+
 for (result in lijst) {  // overloop alle indexen
     console.log("\t%s  \tkomt %d keer voor.", result, lijst[result]);
 }
--- javascript/eige_oef/array/oefAssocArray02.js
+++ javascript/eige_oef/array/oefAssocArray02.js
@@ -1,62 +1,18 @@
 'use strict';
-
 var toetsenbord = require('readline-sync');
 
-var lijstG = new Array();
-var lijstP = new Array();
-var gemeentenaam;
-var postnummer;
-
-
-gemeentenaam = toetsenbord.question("Geef een gemeentenaam of type 'stop' om te sluiten: ");
-while (gemeentenaam != "stop") {
-
-
-    if (lijstG[gemeentenaam] == null) {
-        postnummer = toetsenbord.question("Geef een postnummer: ");
-        lijstG[gemeentenaam] = 0;
-        lijstG[gemeentenaam]++;
-        lijstP[postnummer] = 0;
-        lijstP[postnummer]++;
-    } else {
-        console.log("%s komt %d keer voor.", gemeentenaam, postnummer);
+var postnrs = [];
+var gemeente = toetsenbord.question('Gemeente? ').toUpperCase();
+while(gemeente != 'STOP'){
+    if(postnrs[gemeente]){
+        console.log("Het postnummer van %s is %s", gemeente, postnrs[gemeente]);
     }
-
-    gemeentenaam = toetsenbord.question("Geef een gemeentenaam of type 'stop' om te sluiten: ");
+    else{
+         postnrs[gemeente] = toetsenbord.question("Gemeente niet gevonden.  Postnummer? ");
+    }
+    gemeente = toetsenbord.question('gemeente: ').toUpperCase();
 }
-
-for (gemeentenaam in lijstG) {  // overloop alle indexen
-    console.log("%s komt %d keer voor.", gemeentenaam, lijstG[gemeentenaam]);
+console.log('Overzicht gemeentes:');
+for(gemeente in postnrs){
+    console.log('%s %s', postnrs[gemeente], gemeente);
 }
-for (postnummer in lijstP) {  // overloop alle indexen
-    console.log("%s komt %d keer voor.", postnummer, lijstG[postnummer]);
-}
-
-
-// 'use strict';
-
-// var toetsenbord = require('readline-sync');
-
-// var lijstG = new Array();
-// //var lijstP= new Array();
-// var gemeentenaam;
-// var postnummer;
-
-
-// gemeentenaam = toetsenbord.question("Geef een gemeentenaam of type 'stop' om te sluiten: ");
-// while (gemeentenaam != "stop") {
-
-//     if (lijstG[gemeentenaam] == null){
-//         postnummer = toetsenbord.question("Geef een postnummer: ");
-//         lijstG[gemeentenaam] = 0;
-//         lijstG[gemeentenaam]++;
-//     } else{
-//         console.log("%s komt %d keer voor.", gemeentenaam, lijstG[gemeentenaam]);
-//     }
-
-//     gemeentenaam = toetsenbord.question("Geef een gemeentenaam of type 'stop' om te sluiten: ");
-// }
-
-// for (gemeentenaam in lijstG) {  // overloop alle indexen
-//     console.log("%s komt %d keer voor.", gemeentenaam, lijstG[gemeentenaam]);
-// }
