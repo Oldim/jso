bbd0315a465bc6ef3b39093cdfc1ca81fe67db7c
Oldim <olifirenko88@gmail.com>
2017-11-23 13:32:54 +0100

    test

--- javascript/eige_oef/oop/mycar02.js
+++ javascript/eige_oef/oop/mycar02.js
@@ -1,5 +1,7 @@
 'use strict';
-//functie die we kunnen gebruiken om 0.1 of meer objecten met dezelfde properties en methodes te kunnen maken
+
+// functie die we kunnen gebruiken om 0,1 of meer objecten 
+// met dezelfde properties en methodes te kunnen maken
 // CONSTRUCTOR
 function Auto(merk, model) {
     this.merk = merk;
@@ -7,29 +9,40 @@ function Auto(merk, model) {
     this.cabrio = false;
     this.bouwjaar = new Date().getFullYear();
     this.gestart = false;
-    this.gegevens= function(){
-        var result= this.merk+' , '+ this.model+' , '+ this.bouwjaar+' , '+ this.cabrio;
-        return result;
-    }
     this.starten = function () {
         this.gestart = true;
-        console.log("de motor is gestaart");
+        console.log("de motor draait ....");
     };
-    this.rijden= function(geluid){
-        if(this.gestart){        
-            var tmp= this.gegevens();
-            console.log("Ik vertrek met mijn %s",tmp);
+    this.rijden = function (geluid) {
+        if (this.gestart) {
+            var tmp = this.gegevens();
+            console.log("Ik vertrek met mijn %s", tmp);
             console.log(geluid);
-        }else{
-            console.log("staart de motor eerst")
+        }
+        else {
+            console.log("start de motor eerst!");
         }
     };
+    this.gegevens = function () {
+        var cabrioMelding = this.cabrio ? "cabrio " : "";
+        return cabrioMelding + this.merk + " " + this.model + " uit " + this.bouwjaar;
+    };
 }
+
 // objecten maken met constructor
-var auto1 = new Auto("bmw", "X6");
-var auto2 = new Auto("Audi", "A6");
+var auto1 = new Auto("BMW", "X6");
 console.log(auto1);
-console.log(auto2);
+console.log("auto1 is een " + auto1.merk + " " + auto1.model);
+auto1.rijden("broembroem");
+auto1.starten();
+auto1.rijden("broembroem");
+console.log(auto1.gegevens());
+
+var auto2 = new Auto("Audi", "A6");
 auto2.cabrio = true;
 auto2.starten();
 auto2.rijden("tuuttuut");
+auto2.kleur = "safierzwart";
+
+console.log(auto1);
+console.log(auto2);
--- javascript/eige_oef/oop/oefOop01.js
+++ javascript/eige_oef/oop/oefOop01.js
@@ -31,3 +31,9 @@ for(var i = 0; i < honden.length; i++) {
     console.log(honden[i].blaf());
 };
 
+function Tree(name) {
+    this.name = name;
+  }
+  
+  var theTree = new Tree('Redwood');
+  console.log('theTree.constructor is ' + theTree.constructor);
