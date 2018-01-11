4d10ce3341b301bcfb5396d3181e037c5ae6b8d6
Oldim <olifirenko88@gmail.com>
2018-01-11 14:55:42 +0100

    server_expr

---
+++ Node.js/eigen_oef/server_express/server-express0.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express');
+var server = express();
+
+server.get('/process-form.html', function(request, response){
+    response.sendFile(__dirname+"/process-form.html");
+});
+
+//request methode =GET
+// path: eingigd op .html(maar is niet )
+server.get('/*.html', function(request, response){
+    console.log(" request met method get for html gate received");
+    response.send('request met get voor html pagina wotdt verweerkt');
+});
+//request methode =GET
+// path: eingigd op .html(maar is niet )
+
+server.get('/*', function(request, response){
+
+    console.log(" request met method get for NOT html receivet");
+    response.send('request met get voor NIET-html pagina wotdt verweerkt');
+
+});
+
+server.post('/*', function(request, response){
+    console.log('request met methode post received'),
+    response.send("request voor POST wordt verwerkt")
+
+})
+
+// all: antwoord op elke request van http
+// path: eender wat
+server.all("/*", function(request, response){
+    response.send("Hello server express");
+});
+
+server.listen(8083);
